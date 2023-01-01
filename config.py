4600e46ee8c513c7c8d3bdd5620c6f6738ffa481
Wellington Ameida <64033017+wellyington@users.noreply.github.com>
2023-01-01 11:54:57 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+# MySQL Configurations
+
+host = "localhost"
+database = "emailscraper"
+myuser = "username"
+mypass = "password"
+
+# Badwords
+
+badwords = ['sentry', 'deliveroo', '.png', '.webp', '.jpg', 'x22', 'u003', '.gif', 'trustpilot']
+badwords.extend(['.js', '.heic',  '.jpeg', 'godaddy', 'name', 'example.com', 'domain.com'])
+badwords.extend(['email', 'username', 'opencart', 'zipify', '.global', 'EXCLUDING', '.gov'])
+badwords.extend(['Q@3SU.DP', 'm@X.W', 'IS@k.v', 'lite.lt', 'template', 'checkout', 'widgets'])
+badwords.extend(['example', 'company.com', 'i@+.M', 'Im@K.Z', 'O@i.a', 'q@A.q', 'w@Z.H,', 'Wz6@V.n'])
+badwords.extend(['Z@U.D', 'G@EIr.u', 'Z@U.D', 'Wz6@V.n', 'w@Z.H', 'V@b.T', 'q@A.q', 'O@i.a', 'Im@K.Z'])
+#badwords.extend([''])
