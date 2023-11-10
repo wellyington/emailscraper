efa7c85f5b57b5f1de85d740834b78df411073e8
Wellington Ameida <64033017+wellyington@users.noreply.github.com>
2023-11-10 12:09:18 +0100

    SQLite3 Increments

--- scraper.py
+++ scraper.py
@@ -1,41 +1,52 @@
 #!/bin/python3
-from hashlib import new
-# pip3 install bs4
 from bs4 import BeautifulSoup
 import requests
 import requests.exceptions
 import urllib.parse
 from collections import deque
 import re
-# pip3 install google
 from googlesearch import search
 import datetime
-#pip3 install mysql-connector
-import mysql.connector
-#pip3 install xlswriter
+import sqlite3
 import xlsxwriter
 import os
-from config import host, database, myuser, mypass, badwords
+from config import badwords
 
+# Create a SQLite database connection
+conn = sqlite3.connect('emails.db')
+cursor = conn.cursor()
 
-# MySQL Connector
-
-myconn = mysql.connector.connect(host=host, database=database, user=myuser, password=mypass)
-
+# Create the emails table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS emails (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    searchterm TEXT,
+    email TEXT,
+    date TEXT,
+    time TEXT
+)
+''')
+g_count = 0
 gquery = input('[+] Search Term: ')
 uTotalSearch = input('[+] Number Of URL Results: ')
 uTotalScan = input('[+] Number Of Pages To Scan: ')
 gtotal = int(uTotalSearch)
 gurls = []
+
+# Create a workbook and add a worksheet
+workbook = xlsxwriter.Workbook(f'./xls/{gquery}.xlsx')
+worksheet = workbook.add_worksheet()
+
 for j in search(gquery, tld="com", num=int(gtotal), stop=int(gtotal), pause=2):
     gurls.append(j)
+
 print('Collecting URLs')
 print('-' * 80)
-g_count = 0
 
 for k in gurls:
     g_count += 1
     print('[' + str(g_count) + '] ' + k)
+
 user_total = int(uTotalScan)
 scrapped_urls = set()
 emails = set()
@@ -49,74 +60,66 @@ for user_urls in gurls:
             if count == int(user_total):
                 count = 0
                 break
-            
+
             count += 1
             url = urls.popleft()
             scrapped_urls.add(url)
-            parts =  urllib.parse.urlsplit(url)
+            parts = urllib.parse.urlsplit(url)
             base_url = '{0.scheme}://{0.netloc}'.format(parts)
-            path = url[:url.rfind('/')+1] if '/' in parts.path else url
-            print('[%d] Processing %s' % (count,url))
-            
+            path = url[:url.rfind('/') + 1] if '/' in parts.path else url
+            print('[%d] Processing %s' % (count, url))
+
             try:
                 response = requests.get(url)
             except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
                 continue
-            
+
             new_emails = set(re.findall(r'[a-z0-9\.\-+]+@[a-z0-9\.\-+]+\.[a-z]+', response.text, re.I))
             emails.update(new_emails)
 
             for l in emails:
-                NULL_ = None
                 date_time = datetime.datetime.now()
                 _date = str(date_time.date())
                 _time = str(date_time.strftime("%X"))
-                cursor = myconn.cursor(buffered=True)
-                emailBulk = (NULL_, gquery, l, _date, _time)
-                sql = "INSERT INTO `emailbulk`(`ID`,`searchterm`,`email`,`date`,`time`) VALUES(%s,%s,%s,%s,%s)"
-                cursor.execute(sql,emailBulk)
-                myconn.commit()
-            
+                emailBulk = (gquery, l, _date, _time)
+                cursor.execute("INSERT INTO emails (searchterm, email, date, time) VALUES (?, ?, ?, ?)", emailBulk)
+                conn.commit()
+
             soup = BeautifulSoup(response.text, features='lxml')
-            
+
             for anchor in soup.find_all('a'):
                 link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
                 if link.startswith('/'):
                     link = base_url + link
                 elif not link.startswith('http'):
                     link = path + link
-                if not link in urls and not link in scrapped_urls:
+                if link not in urls and link not in scrapped_urls:
                     urls.append(link)
     except KeyboardInterrupt:
         count = 0
-        print('[-] Closing"')
+        print('[-] Closing')
+
 print('-' * 80)
 for mail in emails:
     print(mail)
-    NULL_ = None
     date_time = datetime.datetime.now()
     _date = str(date_time.date())
     _time = str(date_time.strftime("%X"))
-    cursor = myconn.cursor(buffered=True)
-    emailsScrapped = (NULL_, gquery, mail, _date, _time)
-    sql = "INSERT INTO `emails`(`ID`,`searchterm`,`email`,`date`,`time`) VALUES(%s,%s,%s,%s,%s)"
-    cursor.execute(sql,emailsScrapped)
-    myconn.commit()
+    emailsScrapped = (gquery, mail, _date, _time)
+    cursor.execute("INSERT INTO emails (searchterm, email, date, time) VALUES (?, ?, ?, ?)", emailsScrapped)
+    conn.commit()
 
 noDel = 0
 for badword in badwords:
-
     check = badword
-
-    cursor = myconn.cursor(buffered=True)
-    sqlAdd = "select id, email From emails where email like '%" + check + "%'"
+    sqlAdd = f"SELECT ID, email FROM emails WHERE email LIKE '%{check}%'"
     cursor.execute(sqlAdd)
     toDelete = cursor.fetchall()
     for tdel in toDelete:
-        print(str(tdel[0]) + " : " + tdel[1])
-        sqlDelete = "DELETE FROM emails WHERE id = " + str(tdel[0])
+        print(f"{tdel[0]} : {tdel[1]}")
+        sqlDelete = f"DELETE FROM emails WHERE ID = {tdel[0]}"
         cursor.execute(sqlDelete)
-        myconn.commit()
+        conn.commit()
         noDel += 1
 
 print('-' * 80)
@@ -124,13 +127,9 @@ totalemails = len(emails) - noDel
 lenemails = len(emails)
 print(f'Collected: {lenemails} Emails!')
 print(f'Deleted: {noDel} Invalid Emails!')
-print(f'Total: {totalemails} Valid Emails!' )
+print(f'Total: {totalemails} Valid Emails!')
 
-# Create a workbook and add a worksheet.
-workbook = xlsxwriter.Workbook(f'./xls/{gquery}.xlsx')
-worksheet = workbook.add_worksheet()
-
-sqlemails = "SELECT searchterm, email From emails Where searchterm = '" + gquery + "' Group By email Order By email Asc"
+sqlemails = f"SELECT searchterm, email FROM emails WHERE searchterm = '{gquery}' GROUP BY email ORDER BY email ASC"
 cursor.execute(sqlemails)
 result = cursor.fetchall()
 
@@ -140,9 +139,9 @@ col = 0
 for i in result:
     term = i[0]
     mail = i[1]
-    worksheet.write(row, col,     i[0])
+    worksheet.write(row, col, i[0])
     worksheet.write(row, col + 1, i[1])
-    row += 1   
+    row += 1
 
 worksheet.write(row, 0, 'Total')
 worksheet.write(row, 1, row)
@@ -150,3 +149,4 @@ print('-' * 80)
 print(f'{row} Emails Imported to {os.getcwd()}/xls/{gquery}.xlsx')
 print('-' * 80)
 workbook.close()
+conn.close()
